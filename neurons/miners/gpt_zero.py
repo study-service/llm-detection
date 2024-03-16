de0afb5263784899df91559209fd5eb32b34fd3a
alexnguyen1606 <48585657+alexnguyen1606@users.noreply.github.com>
2024-03-16 22:56:35 +0700

    Update to use default

--- neurons/miners/gpt_zero.py
+++ neurons/miners/gpt_zero.py
@@ -40,47 +40,47 @@ class GPT2PPL:
         return (100 - ppl) / 100
 
     def getPPL(self, sentence):
-        # encodings = self.tokenizer(sentence, return_tensors="pt")
-        # seq_len = encodings.input_ids.size(1)
-        #
-        # nlls = []
-        # likelihoods = []
-        # prev_end_loc = 0
-        # for begin_loc in range(0, seq_len, self.stride):
-        #     end_loc = min(begin_loc + self.max_length, seq_len)
-        #     trg_len = end_loc - prev_end_loc
-        #     input_ids = encodings.input_ids[:, begin_loc:end_loc].to(self.device)
-        #     target_ids = input_ids.clone()
-        #     target_ids[:, :-trg_len] = -100
-        #
-        #     with torch.no_grad():
-        #         outputs = self.model(input_ids, labels=target_ids)
-        #         neg_log_likelihood = outputs.loss * trg_len
-        #         likelihoods.append(neg_log_likelihood)
-        #
-        #     nlls.append(neg_log_likelihood)
-        #
-        #     prev_end_loc = end_loc
-        #     if end_loc == seq_len:
-        #         break
-        #
-        # if torch.isnan(torch.Tensor(nlls)).any() or len(nlls) == 0:
-        #     return None
-        #
-        # ppl = int(torch.exp(torch.stack(nlls).sum() / end_loc))
-        # return ppl
+        encodings = self.tokenizer(sentence, return_tensors="pt")
+        seq_len = encodings.input_ids.size(1)
+
+        nlls = []
+        likelihoods = []
+        prev_end_loc = 0
+        for begin_loc in range(0, seq_len, self.stride):
+            end_loc = min(begin_loc + self.max_length, seq_len)
+            trg_len = end_loc - prev_end_loc
+            input_ids = encodings.input_ids[:, begin_loc:end_loc].to(self.device)
+            target_ids = input_ids.clone()
+            target_ids[:, :-trg_len] = -100
+
+            with torch.no_grad():
+                outputs = self.model(input_ids, labels=target_ids)
+                neg_log_likelihood = outputs.loss * trg_len
+                likelihoods.append(neg_log_likelihood)
+
+            nlls.append(neg_log_likelihood)
+
+            prev_end_loc = end_loc
+            if end_loc == seq_len:
+                break
+
+        if torch.isnan(torch.Tensor(nlls)).any() or len(nlls) == 0:
+            return None
+
+        ppl = int(torch.exp(torch.stack(nlls).sum() / end_loc))
+        return ppl
 
         # used fix bug by gpt
-        encodings = self.tokenizer(sentence, return_tensors="pt")
-        input_ids = encodings.input_ids.to(self.device)
-        with torch.no_grad():
-            outputs = self.model(input_ids)
-            logits = outputs.logits
-            # Calculate perplexity
-            log_probs = torch.log_softmax(logits, dim=-1)
-            token_likelihoods = log_probs.gather(dim=-1, index=input_ids.unsqueeze(-1)).squeeze(-1)
-            perplexity = torch.exp(-token_likelihoods.mean())
-        return perplexity.item()
+        # encodings = self.tokenizer(sentence, return_tensors="pt")
+        # input_ids = encodings.input_ids.to(self.device)
+        # with torch.no_grad():
+        #     outputs = self.model(input_ids)
+        #     logits = outputs.logits
+        #     # Calculate perplexity
+        #     log_probs = torch.log_softmax(logits, dim=-1)
+        #     token_likelihoods = log_probs.gather(dim=-1, index=input_ids.unsqueeze(-1)).squeeze(-1)
+        #     perplexity = torch.exp(-token_likelihoods.mean())
+        # return perplexity.item()
 
 
 if __name__ == '__main__':
